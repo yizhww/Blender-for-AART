cd7ae9d0f9fe9b6988d711d83c3cf0b82a2f6e76
AART <yzh_1234@qq.com>
2025-04-25 09:55:42 +0800

    test

--- convert_toml_to_json.py
+++ convert_toml_to_json.py
@@ -45,7 +45,7 @@ def convert_toml_to_json():
             "blender_version_max": tomd.get("blender_version_max", ""),
             "website": tomd.get("website", ""),
             "tags": tomd.get("tags", []),
-            "archive_url": f"./my extension/{tomd['id']}/{tomd['id']}.zip",
+            "archive_url": f"./my%20extension/{tomd['id']}/{tomd['id']}.zip",
             "archive_size": zip_size,
             "archive_hash": f"sha256:{zip_hash}"
         }
